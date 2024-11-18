ee6139e617ac712fd50aee12980d5f2e278f0a12
rahu186 <gamingerag@gmail.com>
2024-11-18 19:14:40 +0530

    Your new commit message

--- job-portal/vite.config.js
+++ job-portal/vite.config.js
@@ -3,6 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+
+  build: {
+    rollupOptions: {
+      external: ['react-quill']
+    }
+  },
   plugins: [react()],
   resolve: {
     alias: {
