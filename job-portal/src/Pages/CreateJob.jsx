0a4feb538cb7ead68f28f04310474198c202377e
rahu186 <gamingerag@gmail.com>
2024-11-18 19:49:16 +0530

    Your new commit message

--- job-portal/src/Pages/CreateJob.jsx
+++ job-portal/src/Pages/CreateJob.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import Creatable from "react-select/creatable";
-import ReactQuill from "react-quill";
-import "react-quill/dist/quill.snow.css";
+// import ReactQuill from "react-quill";
+// import "react-quill/dist/quill.snow.css";
 
 const CreateJob = () => {
   const [selectedOption, setSelectedOption] = useState(null);
