15b5ed538aaedb34fab322a41fd90d010d1631de
rahu186 <gamingerag@gmail.com>
2024-11-18 19:27:08 +0530

    Your new commit message

--- job-portal/vite.config.js
+++ job-portal/vite.config.js
@@ -3,11 +3,12 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
   build: {
     rollupOptions: {
-      external: ['react-quill']
-    }
+      external: [
+        'react-quill/dist/quill.snow.css', // Externalize the CSS file
+      ],
+    },
   },
   plugins: [react()],
   resolve: {
